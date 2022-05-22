9eee3f0b29db24844713ca5463c132bd87e85fc0
Dayvid <@dayvid7santos2002@outlook.com>
2022-05-22 17:40:55 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import  InitialPart from './componentes/InitialPart'
+
+export default function (){
+  return(
+    <View>
+    <InitialPart></InitialPart>
+    <Text>warup</Text>
+    </View>
+    )}
+
+const styles = StyleSheet.create({
+
+});
+// cara é muito melhor de aprender, quando você mesmo começa a fazer a parada sem copiar de video aula
+// nem nada, você erra,e ja sabe do seu erro, não do erro dos outros, isso é foda.
+// nunca esqueça disso.
---
+++ src/componentes/InitialPart.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+export default function (){
+  return( 
+  <View style={styles.container} >
+      <View style={{height: ('5%')}} > 
+        <Text style={styles.lettersOne}  >Dayvid</Text>
+        <Text style={{  alignSelf:'center'}} >_________</Text>
+        <Text style={styles.lettersTwo} >HARD</Text>
+    </View>
+       <View style={{justifyContent:'space-evenly'}}  >
+       <Text style={styles.lettersThree}>TRUTH</Text>
+       <Text style={{  alignSelf:'center'}} >  _________</Text>
+       <Text style={{fontSize:28,textAlign:'center',color:('#De2674'), fontWeight:('300')}}
+            >RANDOM CHANCE</Text>
+       <Text style={{  alignSelf:'center'}} >  _________</Text>
+       <Text style={styles.lettersThree}>DARE</Text>
+     </View>
+     
+     <View style={{flexDirection:'row'}} >
+          <Text style={{fontSize:25, textAlign:'center', color:'white', width:'80%',
+        fontWeight:'300'
+        }}>Include "Custom"?</Text>
+     </View>
+
+  </View>
+     
+)}
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: (`#000000`),
+    height:'100%',
+    padding:20,
+    justifyContent:'space-between'
+    
+  },
+  lettersOne:{
+      fontSize:30,
+      color:('#ff09de'),
+      alignSelf:'center',
+      
+  },
+  lettersTwo:{
+    fontSize:20,
+    color:('#ff66eb'),
+    color:('#be2596'),
+    fontStyle:('normal'),
+    fontWeight:('100'),
+    alignSelf:'center'
+  },
+  lettersThree:{
+    fontSize:40,
+    color:('#ff09de'),
+    alignSelf:'center',
+    
+
+}
+});
+// cara é muito melhor de aprender, quando você mesmo começa a fazer a parada sem copiar de video aula
+// nem nada, você erra,e ja sabe do seu erro, não do erro dos outros, isso é foda.
+// nunca esqueça disso.
+
+// ULTRA IMPORTANTE. 
+// um monte de download de pacote não da certo porque você tá com o metro a todo vapor.
+// sempre que for instalar pacotes, faça o ctrl C para terminar o metro, e instalar os pacotes
+// porque senão da merda no app. deve ter sido por isso que não instalava as dependencias
+// do app lá
